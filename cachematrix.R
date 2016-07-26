6266b3e7873ad31ceeec82c49257447b390d63ae
AlejandroCastilloCruz <a.castillo.cruz.1989@gmail.com>
2016-07-26 14:04:01 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,27 @@
-## Create a Matrix equal *2
-> A = matrix( c(1,2,3,4), nrow=2 ) *2 
-## Execute and return the inverse result
-> solve(A) 
-     [,1]  [,2]
-[1,] -1.0  0.75
-[2,]  0.5 -0.25
-
-## Creating a matrix wich is returning cache of data 
-cachemean <- function(D, ...) {
-        
-P <- D$getmean()
-        
-if(!is.null(P)) {
-                
-message("Cache from the data")
-                
-return(P)
-        
+makeCacheMatrix<- function(x=matrix()) {
+        A <- NULL
+        set <- function(y) {
+                x <<- y
+                A <<- NULL
+        }
+        get <- function() x
+        setcache <- function(solve) A <<- solve
+        getcache <- function() A
+        list(set = set, get = get,
+             setcache = setcache,
+             getcache = getcache)
 }
-        
-data <- D$get()
-        
-m <- mean(data, ...)
-        
-D$setmean(D)
-        
-D
+
+
+## CacheSolve
+cacheSolve <- function(x,...) {
+        A <- x$getcache()
+        if(!is.null(A)) {
+                message("getting cached data")
+                return(A)
+        }
+        data <- x$get()
+        A <- solve(data)
+        x$setcache(A)
+        A
 }
